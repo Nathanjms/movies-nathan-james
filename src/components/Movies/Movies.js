89316b62e39e3c414021fe84a2d1a1fffa9debea
Nathan James <nathanjms@outlook.com>
2021-08-23 20:08:36 +0100

    Add todo

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -66,7 +66,7 @@ export default function Movies({ currentUser }) {
       );
       const resultAllUnseen = await AuthenticatedRequest(currentUser).get(
         `/api/movies/${userGroupId}/group?isSeen=1&perPage=200`
-      );
+      ); //TODO: Get this array when doing the random movie selection.
 
       setMySeenMovies(resultSeen.data);
       setMyUnseenMovies(resultUnseen.data);
